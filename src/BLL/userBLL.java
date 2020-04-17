b33a30f4ff576b93f45c400d443348dd02bc1a8e
EndUser <EndUser@192.168.1.4>
2020-04-17 16:12:47 +0700

    UserBLL

--- src/BLL/userBLL.java
+++ src/BLL/userBLL.java
@@ -2,4 +2,36 @@ package BLL;
 
 public class userBLL {
 
+	
+	private int userID;
+	private String username;
+	private String fullname;
+	private int age;
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+
 }
